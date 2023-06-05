4ebf0137060eff8a1b24a476d2363fc82580c26e
Avidan123 <75628503+Avidan123@users.noreply.github.com>
2023-06-05 12:51:37 +0530

    Add files via upload

---
+++ multidimentional.py
@@ -0,0 +1,15 @@
+fruits=["apples","oranges","mango","banana"]
+print(fruits[1])
+
+subjects=[["maths",98],["physics",100],["chemistry",100],["history",90],["english",97]
+          ,["computer",94],["PE",96]]
+print(subjects[2][0])
+print(subjects[5][0])
+print(subjects[5][1])
+
+
+person=[[["mohan","45","male"],["rohna","25","female"],["mohit","23","male"]],
+        [["mohir","60","male"],["manorma","35","male"],["mohim","15","male"]]]
+
+print(person[0][2][1])
+print(person[1][2][0])
